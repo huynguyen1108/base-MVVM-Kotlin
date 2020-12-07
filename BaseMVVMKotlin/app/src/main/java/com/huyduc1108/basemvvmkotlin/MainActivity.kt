7f7c41ae7fbe7ac32767fd957aeb45a3da0b0045
Huynd <huynd@ttc-solutions.com.vn>
2020-12-07 13:57:24 +0700

    init project

---
+++ BaseMVVMKotlin/app/src/main/java/com/huyduc1108/basemvvmkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.huyduc1108.basemvvmkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
